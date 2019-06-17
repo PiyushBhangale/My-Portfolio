8e7221fa36356a4b56954bc436ead927146de994
PiyushBhangale <piyushbhangale1995@gmail.com>
2019-06-17 12:12:14 +0530

    final commit

---
+++ src/resumeData.js
@@ -0,0 +1,135 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Piyush Bhangale",
+  role: "Fullstack Developer",
+  linkedinId: " +91 8556533390",
+
+  roleDescription:
+    " I like dabbling in various parts of software development and like to learn about new technologies, sometimes write some articles or simply bump into Reddit or Quora in free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/piyush-bhangale-68a943b9/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/PiyushBhangale/",
+      className: "fa fa-github"
+    },
+    {
+      name: "skype",
+      url: "https://twitter.com/piyushbhangale1",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme:
+    "Software Engineer specializing in backend, infrastructure and web development. One who loves Mac, Linux and enjoys to customize all of the development environment. Interested in devising a better problem-solving method for challenging tasks, and learning new technologies and tools if the need arises. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  website: "piyushbhangale1995@gmail.com",
+
+  education: [
+    {
+      UniversityName: "Pune Institute Of Computer Techonology",
+      specialization: "BE (Information Techonology)",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2017"
+      // Achievements: "Some Achievements"
+    }
+    // {
+    //   UniversityName: "Some University",
+    //   specialization: "Some specialization",
+    //   MonthOfPassing: "Jan",
+    //   YearOfPassing: "2018",
+    //   Achievements: "Some Achievements"
+    // }
+  ],
+  work: [
+    {
+      CompanyName: "Sattvarise Technologies (Intern)",
+      specialization: "Vuforia, Google Carboard, AR/VR applicatons, Unity",
+      MonthOfLeaving: "March",
+      YearOfLeaving: "2017",
+      Achievements:
+        "The role included development of Augmented Reality and Virtual Reality related applications with Unity platform."
+    },
+    {
+      CompanyName: "Trendzlink (Intern)",
+      specialization: "Python, Django, JavaScript, Css, HTML, Mysql",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "The role included development of website and a chatbot."
+    },
+    {
+      CompanyName: "LiveHealth (Jr Software Developer)",
+      specialization: "Python, Django, ReactJs, Css, HTML, Mysql",
+      MonthOfLeaving: "Dec",
+      YearOfLeaving: "2018",
+      Achievements:
+        "Developed various modules for overall enhancement of Livehealth as a product."
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    ,
+    {
+      skillname: "JavaScript"
+    },
+    ,
+    {
+      skillname: "Python"
+    },
+    {
+      skillname: "Bash-scripting"
+    },
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "Reactjs"
+    },
+    {
+      skillname: "CSS"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Desktop Cricket Notifier",
+      description:
+        "A desktop notifier is an application which produces a notification regarding the scores of the ongoing cricket matches on desktop.",
+      imgurl: "images/portfolio/phone.jpg",
+      link: "https://github.com/PiyushBhangale/Desktop-Cricket-notifier"
+    },
+    {
+      name: "Spotify Playlist Generator",
+      description:
+        "Spotify Playlist Generator is a Python script that automatically builds a new Spotify playlist each week with new music.",
+      imgurl: "images/portfolio/project2.png",
+      link: "https://github.com/PiyushBhangale/Spotify-generator"
+    },
+    {
+      name: "Chai Rasik",
+      description: "A online shopping website for Chai related goods.",
+      imgurl: "images/portfolio/chairasik.jpg",
+      link: "https://github.com/PiyushBhangale/ChaiRasik"
+    },
+
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "Talk is cheap. Show me the code.",
+      name: "Linus Torvalds"
+    },
+    {
+      description:
+        "Bad programmers worry about the code. Good programmers worry about data structures and their relationships.",
+      name: "Linus Torvalds"
+    }
+  ]
+};
+
+export default resumeData;
