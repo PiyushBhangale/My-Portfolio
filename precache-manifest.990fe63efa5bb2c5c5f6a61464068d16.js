d4bcdba8cfb8aa61ad52ceeb1cfc0e80acf0f84b
PiyushBhangale <piyushbhangale1995@gmail.com>
2019-06-17 12:19:32 +0530

    Updates

---
+++ precache-manifest.990fe63efa5bb2c5c5f6a61464068d16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a8e442a489a64e6b800",
+    "url": "/static/js/main.1a8e442a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "1a8e442a489a64e6b800",
+    "url": "/static/css/main.8a155958.chunk.css"
+  },
+  {
+    "revision": "467e6f728c0f85efa5fb6772aad5b209",
+    "url": "/index.html"
+  }
+];
